dae2cc857f96eb9b3cd191350f208f869e1a9b4a
JohnAnon9771 <njoao97710@gmail.com>
2020-09-27 17:55:02 -0300

    :tada: add prettier, eslint, parcel and more tools

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import { Pet } from './Pet';
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
+  return React.createElement('div', { id: 'something-important' }, [
+    React.createElement('h1', {}, 'Adopt Me!'),
     React.createElement(Pet, {
-      name: "Ruck",
-      animal: "Dog",
-      breed: "Havanese",
+      name: 'Ruck',
+      animal: 'Dog',
+      breed: 'Havanese',
     }),
     React.createElement(Pet, {
-      name: "Ruck",
-      animal: "Dog",
-      breed: "Havanese",
+      name: 'Ruck',
+      animal: 'Dog',
+      breed: 'Havanese',
     }),
     React.createElement(Pet, {
-      name: "Ruck",
-      animal: "Dog",
-      breed: "Havanese",
+      name: 'Ruck',
+      animal: 'Dog',
+      breed: 'Havanese',
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById('root'));
