0085c2c4dc9050e7f823c4e3271bf0e3ff73384b
JohnAnon9771 <njoao97710@gmail.com>
2020-09-28 18:23:20 -0300

    :tada: add hooks

--- intro-to-react-v5/src/SearchParams.js
+++ intro-to-react-v5/src/SearchParams.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 function SearchParams() {
   const [location, setLocation] = useState('Seattle, WA');
-  const [animal, setAnimal] = useState('Dog');
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'Dog', ANIMALS);
+  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
 
   return (
     <div className="search-params">
@@ -18,22 +21,8 @@ function SearchParams() {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animals">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
